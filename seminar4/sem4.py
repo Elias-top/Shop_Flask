65d4950d5cf2dd213e7534180c5621d148741d96
Elias-top <bkmz1999ilyxa@gmail.com>
2024-07-24 20:39:46 +0300

    добавил возможность передавать задавать список URL-адресов через аргументы командной строки

--- seminar4/sem4.py
+++ seminar4/sem4.py
@@ -5,6 +5,7 @@ import aiohttp
 from concurrent.futures import ProcessPoolExecutor
 from concurrent.futures import ThreadPoolExecutor
 from urllib.parse import urlparse
+import argparse
 
 # Директория для сохранения изображений
 DOWNLOAD_DIR = "downloaded_images"
@@ -14,12 +15,14 @@ if not os.path.exists(DOWNLOAD_DIR):
     os.makedirs(DOWNLOAD_DIR)
 
 # Предопределённый список URL-адресов
-URLS = [
-    "https://cdn.pixabay.com/photo/2023/10/26/04/52/old-8341706_640.jpg",
-    "https://cdn.pixabay.com/photo/2024/06/19/05/37/animal-8839173_640.jpg",
-    "https://cdn.pixabay.com/photo/2024/03/31/06/17/googles-8666108_640.jpg"
-]
+# URLS = [
+#     "https://cdn.pixabay.com/photo/2023/10/26/04/52/old-8341706_640.jpg",
+#     "https://cdn.pixabay.com/photo/2024/06/19/05/37/animal-8839173_640.jpg",
+#     "https://cdn.pixabay.com/photo/2024/03/31/06/17/googles-8666108_640.jpg"
+# ]
 
+#Для запуска программы с передачей URL-адресов выполните следующую команду:
+#py.exe sem4.py https://cdn.pixabay.com/photo/2023/10/26/04/52/old-8341706_640.jpg https://cdn.pixabay.com/photo/2024/06/19/05/37/animal-8839173_640.jpg https://cdn.pixabay.com/photo/2024/03/31/06/17/googles-8666108_640.jpg
 
 def download_image(url):
     filename = os.path.basename(urlparse(url).path)
@@ -58,6 +61,27 @@ def download_images(urls):
     print(f"Общее время загрузки: {total_time:.2f} секунд")
 
 
-if __name__ == '__main__':
+def main():
+    # Инициализация парсера аргументов командной строки
+    parser = argparse.ArgumentParser(
+        description="Загрузчик изображений с использованием многопроцессорности и асинхронности.")
+
+    # Добавление аргумента для передачи URL-адресов
+    parser.add_argument(
+        "urls",
+        metavar="URL",
+        type=str,
+        nargs="+",
+        help="Список URL-адресов изображений для загрузки."
+    )
+
+    # Парсинг аргументов
+    args = parser.parse_args()
+
+    # Запуск загрузки изображений
     print("Начинаем загрузку изображений...")
-    download_images(URLS)
+    download_images(args.urls)
+
+
+if __name__ == '__main__':
+    main()
